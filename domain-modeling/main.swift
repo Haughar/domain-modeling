6542a321c08c612fccc9a662c61cc8b5cf727994
Ali Haugh <haughar@hotmail.com>
2015-10-14 17:20:26 -0700

    Initial Commit

---
+++ domain-modeling/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  domain-modeling
+//
+//  Created by Alison Rose Haugh on 10/14/15.
+//  Copyright © 2015 Alison Haugh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
